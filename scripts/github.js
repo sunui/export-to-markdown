e130a5fab266eea87cd851bb8415527eebbf10cd
sunui <776766759@qq.com>
2019-03-13 16:14:08 +0800

    fix:通过URL传参实现自动填充文件名

--- scripts/github.js
+++ scripts/github.js
@@ -27,15 +27,7 @@ const Page = {
       function(r) {
         console.log(1, r);
         if (r.value) {
-          const filePath = r.value.split("\n");
-          const line = filePath.find(l => l.indexOf("本文永久链接") > -1);
-          console.log(line);
-          const title = line.substring(
-            line.indexOf("TODO1/") + 6,
-            line.indexOf(".md") + 3
-          );
-          console.log(2, title);
-          document.querySelector(".js-blob-filename").value = title || "";
+          
         }
       }
     );
